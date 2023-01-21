6f24711623a940d2c92eb1d470ec4c55dc5c23c1
lukemedlin <41134833+lukemedlin@users.noreply.github.com>
2023-01-20 18:38:34 -0600

    Create computer choice function

--- code.js
+++ code.js
@@ -1,1 +1,35 @@
 console.log("Hello World!");
+
+
+function getComputerChoice () {
+    let randNum = Math.floor(Math.random() * 3);
+    let compChoice;
+
+    switch (randNum) {
+        case 0:
+            compChoice = "Rock";
+            break;
+        case 1:
+            compChoice = "Paper";
+            break;
+        case 2:
+            compChoice = "Scissors";
+    }
+    return compChoice;
+}
+
+
+
+
+
+// Computer chooses rock, paper, or scissors
+// User inputs rock, paper, or scissors
+// 
+// Compare the computer and user guesses
+// 1. Rock beats paper
+// 2. Paper beats rock
+// 3. Scissors beats paper
+// 4. Tie if both guess the same
+//
+// Log whether the computer or user wins
+
