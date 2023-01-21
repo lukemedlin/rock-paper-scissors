fb9adb6aa7a2f82be6e75b3b5f863fe610f4506b
lukemedlin <41134833+lukemedlin@users.noreply.github.com>
2023-01-20 19:21:51 -0600

    Create playRound function

--- code.js
+++ code.js
@@ -15,13 +15,44 @@ function getComputerChoice () {
         case 2:
             compChoice = "Scissors";
     }
+
     return compChoice;
 }
 
+function playRound(playerSelection, computerSelection) {
+    playerSelection = prompt("What do you choose?");
+    computerSelection = getComputerChoice();
+    
+    let firstLetter = playerSelection.slice(0,1).toUpperCase();
+    playerSelection = firstLetter + playerSelection.slice(1).toLowerCase(); 
 
+    let result;
 
+    if (playerSelection === computerSelection) {
+        result = "Tie";
+    } else if (playerSelection === "Rock" && computerSelection === "Paper") {
+        result = "You Lose! Paper beats Rock";
+    } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        result = "You Win! Rock beats Scissors";
+    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+        result = "You Win! Scissors beats Paper";
+    } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+        result = "You Lose! Rock beats Scissors";
+    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+        result = "You Win! Paper beats Rock";
+    } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+        result = "You Lose! Scissors beats Paper";
+    }
+    
+    return result;
+}
 
-
+// if player === rock and 
+// 
+// 
+//  
+//  
+//  
 // Computer chooses rock, paper, or scissors
 // User inputs rock, paper, or scissors
 // 
